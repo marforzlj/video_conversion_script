c97e45c4926462785489f483571ad4afc046ab99
Daniel Ngu <daniel.dy.ngu@gmail.com>
2014-04-05 10:42:23 +1300

    Amended license
    Signed-off-by: Daniel Ngu <daniel.dy.ngu@gmail.com>

--- video_conversion_script.sh
+++ video_conversion_script.sh
@@ -1,19 +1,19 @@
-# video_conversion_script.sh -
+# video_conversion_script.sh
 #
-#  Video Converter - A shell script that utilises ffmpeg to simplify the 
-#		     video conversion process.
+# Video Converter - A shell script that utilises ffmpeg to simplify the 
+#		    video conversion process.
 #
 # Copyright (C) 2011 Daniel Ngu <daniel.dy.ngu@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or (at
-# your option) any later version.
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 #!/bin/sh
 
